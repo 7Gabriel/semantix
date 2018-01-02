c39a107f75fb65412ed8dcfe077c275b943d1f4c
7Gabriel <joao1.gabriel@hotmail.com>
2018-01-02 16:12:50 -0200

    Update hostunico.py

--- hostunico.py
+++ hostunico.py
@@ -53,18 +53,18 @@ def parseDeLogs():
     erroLogCount = erroLogs.count()
    
     if erroLogCount > 0:
-        print 'Numero de linhas invalidas no log: %d' % erroLogs.count()
+        print ('Numero de linhas invalidas no log: %d' % erroLogs.count())
         for linha in erroLogs.take(20):
-            print 'Linha Invalida: %s' % linha
+            print ('Linha Invalida: %s' % linha)
 
-    print 'Total de linhas lidas %d, Total de linhas convertidas com sucesso %d, Total de falhas na conversao %d' % (convertidoLogs.count(), dadosLogs.count(), erroLogs.count())
+    print ('Total de linhas lidas %d, Total de linhas convertidas com sucesso %d, Total de falhas na conversao %d' % (convertidoLogs.count(), dadosLogs.count(), erroLogs.count()))
     return dadosLogs
 
 if __name__ == "__main__":
 
     dadosLogs = parseDeLogs()
     hosts = dadosLogs.map(lambda log: log.host).distinct().count()
-    print 'Numero de hosts unicos: %d' % hosts
+    print ('Numero de hosts unicos: %d' % hosts)
 
 
 
